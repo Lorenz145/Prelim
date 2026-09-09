3233bfee94503303473962b91572f98cecb01849
Lorenz145 <lorenzormichigos1@gmail.com>
2026-09-09 14:25:11 +0800

    Add files via upload

--- doublyLinkedList.java
+++ doublyLinkedList.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package linkedlist;
 
-/**
- *
- * @author admin
- */
+
 public class doublyLinkedList {
+    class Node {
+        
+        int data;
+        Node previous;
+        Node next;
+        
+        Node(int data) {
+            this.data = data;
+            this.previous = null;
+            this.next = null;
+        }
+    }
+    Node head;
     
+    void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        newNode.previous = current;
+    }
+    void display() {
+        Node current = head;
+        
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        doublyLinkedList list = new doublyLinkedList();
+        
+        list.add(30);
+        list.add(60);
+        list.add(90);
+        
+        list.display();
+        
+        
+    }
 }
